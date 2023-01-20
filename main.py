9ae1ec84b914725694c117e80a2e9a797628a869
yuriy-matsay <yuriymatsay@gmail.com>
2023-01-20 17:29:14 +0200

    v0.0.8

--- main.py
+++ main.py
@@ -5,47 +5,30 @@ from kivy.core.window import Window
 from kivymd.uix.menu import MDDropdownMenu
 from kivymd.uix.screenmanager import MDScreenManager
 from kivymd.uix.pickers import MDDatePicker
+import yaml
 
 Window.size = 400, 700
 
 
-class Tab(MDFloatLayout, MDTabsBase):
-    pass
+class MyRoot(MDScreenManager):
+    def return_settings_data(self, value):
+        with open('settings.yaml', 'r') as s_d:
+            data = yaml.safe_load(s_d)
+        return data[value]
 
+    def set_settings_data(self, value):
+        with open('settings.yaml', 'w') as s_d:
+            yaml.dump(value, s_d)
 
+    def cmc(self):
+        pass
 
-class MyApp(MDApp):
+class MyApp(MDApp, MyRoot):
     def build(self):
         self.title = 'myapp'
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "BlueGray"
-
-    def fu(self, instance):
-        print(instance.text)
-        instance.text = '2345'
-
-    def on_save(self, instance, value, date_range):
-        '''
-        Events called when the "OK" dialog box button is clicked.
-
-        :type instance: <kivymd.uix.picker.MDDatePicker object>;
-        :param value: selected date;
-        :type value: <class 'datetime.date'>;
-        :param date_range: list of 'datetime.date' objects in the selected range;
-        :type date_range: <class 'list'>;
-        '''
-        print()
-        print(value)
-
-    def on_cancel(self, instance, value):
-        pass
-
-    def show_date_picker(self):
-        date_dialog = MDDatePicker()
-        date_dialog.bind(on_save=self.on_save, on_cancel=self.on_cancel)
-        date_dialog.open()
-
-
+        return MyRoot()
 
 
 if __name__ == "__main__":
