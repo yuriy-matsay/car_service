4a6d462d43699072911a1c5e8382431b54247d43
yuriy-matsay <yuriymatsay@gmail.com>
2023-01-24 16:42:26 +0200

    v0.1.0

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from kivymd.uix.pickers import MDDatePicker
 from kivy.uix.screenmanager import FadeTransition
 from kivymd.uix.button import MDFillRoundFlatButton
 from kivymd.uix.textfield import MDTextField
+from kivymd.uix.label import MDLabel
 import json
 
 Window.size = 400, 700
@@ -27,9 +28,36 @@ class MyRoot(MDScreenManager):
     def add_tracking_element(self):
         self.ids.mybox.add_widget(MDTextField(hint_text='What will tracking?'))
 
-    def tye(self, value):
-        for i in value:
-            print(i.text)
+    def mybox_build(self):
+        for i in range(len(data['tracking_elements'])):
+            self.ids.mybox.add_widget(MDLabel(text=data['tracking_elements'][i]))
+
+    def on_save(self, instance, value, date_range):
+        '''
+        Events called when the "OK" dialog box button is clicked.
+
+        :type instance: <kivymd.uix.picker.MDDatePicker object>;
+        :param value: selected date;
+        :type value: <class 'datetime.date'>;
+        :param date_range: list of 'datetime.date' objects in the selected range;
+        :type date_range: <class 'list'>;
+        '''
+        value = str(value)
+        val: str = value.replace('-', '/')
+        self.ids.mydate.text = val
+
+    def on_cancel(self, instance, value):
+        '''Events called when the "CANCEL" dialog box button is clicked.'''
+        pass
+
+    def show_date_picker(self):
+        date_dialog = MDDatePicker()
+        date_dialog.bind(on_save=self.on_save, on_cancel=self.on_cancel)
+        date_dialog.open()
+
+    def ppp(self, *args):
+        print(args)
+
 
 class MyApp(MDApp, MyRoot):
     def build(self):
